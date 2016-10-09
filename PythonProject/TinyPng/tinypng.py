4b1801f9a93992d517acb1a33fb1c709ef2fa7ea
sloop <liyulongss@qq.com>
2016-10-10 01:27:27 +0800

    添加语音提示(限Mac用户)

--- PythonProject/TinyPng/tinypng.py
+++ PythonProject/TinyPng/tinypng.py
@@ -6,9 +6,19 @@ import sys
 import os.path
 import click
 import tinify
+import platform
 
 tinify.key = "在此处填写你申请的API"		# API KEY
-version = "1.0.1"				# 版本
+version = "1.1.0"		# 版本号
+voice = True			# 是否语音提示
+
+def say(content):
+	if platform.system() == "Darwin" and voice == True:
+		print content
+		os.system("say '"+content+"' ")
+	else:
+		print content
+	pass
 
 # 压缩的核心
 def compress_core(inputFile, outputFile, img_width):
@@ -21,16 +31,16 @@ def compress_core(inputFile, outputFile, img_width):
 
 # 压缩一个文件夹下的图片
 def compress_path(path, width):
-	print "compress_path-------------------------------------"
+#	print "压缩指定目录下文件!"
 	if not os.path.isdir(path):
-		print "这不是一个文件夹，请输入文件夹的正确路径!"
+		say("骗纸! 这根本不是一个文件夹!")
 		return
 	else:
+		say("尝试压缩, 请稍后!")
 		fromFilePath = path 			# 源路径
 		toFilePath = path+"/tiny" 		# 输出路径
-		print "fromFilePath=%s" %fromFilePath
-		print "toFilePath=%s" %toFilePath
-
+#		print "fromFilePath=%s" %fromFilePath
+#		print "toFilePath=%s" %toFilePath
 		for root, dirs, files in os.walk(fromFilePath):
 			print "root = %s" %root
 			print "dirs = %s" %dirs
@@ -49,18 +59,19 @@ def compress_path(path, width):
 
 # 仅压缩指定文件
 def compress_file(inputFile, width):
-	print "compress_file-------------------------------------"
+#	print "压缩指定文件!"
 	if not os.path.isfile(inputFile):
-		print "这不是一个文件，请输入文件的正确路径!"
+		say("骗纸! 这根本不是文件!")
 		return
-	print "file = %s" %inputFile
+	say("尝试压缩, 请稍后!")
+#	print "file = %s" %inputFile
 	dirname  = os.path.dirname(inputFile)
 	basename = os.path.basename(inputFile)
 	fileName, fileSuffix = os.path.splitext(basename)
 	if fileSuffix == '.png' or fileSuffix == '.jpg' or fileSuffix == '.jpeg':
 		compress_core(inputFile, dirname+"/tiny_"+basename, width)
 	else:
-		print "不支持该文件类型!"
+		say("不支持该文件类型!")
 
 @click.command()
 @click.option('-f', "--file",  type=str,  default=None,  help="单个文件压缩")
@@ -68,16 +79,19 @@ def compress_file(inputFile, width):
 @click.option('-w', "--width", type=int,  default=-1,    help="图片宽度，默认不变")
 def run(file, dir, width):
 	print ("GcsSloop TinyPng V%s" %(version))
+	say("启动压缩魔法!")
 	if file is not None:
+#		say("压缩指定文件!")
 		compress_file(file, width)				# 仅压缩一个文件
 		pass
 	elif dir is not None:
+#		say("压缩指定目录下所有图片文件")
 		compress_path(dir, width)				# 压缩指定目录下的文件
 		pass
 	else:
+#		say("压缩当前目录下所有图片文件")
 		compress_path(os.getcwd(), width)		# 压缩当前目录下的文件
-	print "结束!"
+	say("魔法结束!")
 
 if __name__ == "__main__":
     run()
-
